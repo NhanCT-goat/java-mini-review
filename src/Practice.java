04dce1957c5e7ef2f318587799ef129147963f15
NhanCT-goat <300566518+NhanCT-goat@users.noreply.github.com>
2026-09-23 21:12:24 -0700

    second

--- src/Practice.java
+++ src/Practice.java
@@ -48,6 +48,9 @@ public class Practice {
      */
     public static boolean moreThanDouble(int a, int b) {
         // TODO: Delete the dummy return statement and implement this method here!
+        if (a > 2 * b){
+            return true;
+        }
         return false;
     }
 
